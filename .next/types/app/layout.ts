d57c60d4eb2240e64f6b6893cc9adbfd1ff41d36
Ssentongo Richard <richard.bposeats@gmail.com>
2025-10-24 15:37:38 +0300

    Remove supabase

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/project/src/app/layout.tsx
+// File: /Users/macbookpro2018/Downloads/upn-nextjs/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
